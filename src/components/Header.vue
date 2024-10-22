0379f5349bd98ccaebf03abf99e5dd4eb90aab77
Ashishkhose7 <khoseashish7@gmail.com>
2024-10-22 23:19:06 +0530

    home view

---
+++ src/components/Header.vue
@@ -0,0 +1,240 @@
+<script setup>
+import { onMounted } from 'vue';
+
+defineProps({
+ 
+})
+onMounted(()=>{
+	const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const headerMenu = document.getElementById("header");
+
+// Open Close Navbar Menu on Click Burger
+if (burgerMenu && navbarMenu) {
+   burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("is-active");
+      navbarMenu.classList.toggle("is-active");
+   });
+}
+
+// Close Navbar Menu on Click Menu Links
+document.querySelectorAll(".menu-link").forEach((link) => {
+   link.addEventListener("click", () => {
+      burgerMenu.classList.remove("is-active");
+      navbarMenu.classList.remove("is-active");
+   });
+});
+
+// Change Header Background on Scrolling
+window.addEventListener("scroll", () => {
+   if (window.scrollY >= 85) {
+      headerMenu.classList.add("on-scroll");
+   } else {
+      headerMenu.classList.remove("on-scroll");
+   }
+});
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+   if (window.innerWidth > 768) {
+      if (navbarMenu.classList.contains("is-active")) {
+         navbarMenu.classList.remove("is-active");
+      }
+   }
+});
+})
+
+</script>
+
+<template>
+  <header class="header sticky top-0" id="header">
+   <nav class="navbar container">
+      <a href="/" class="brand">Brand</a>
+      <div class="burger" id="burger">
+         <span class="burger-line"></span>
+         <span class="burger-line"></span>
+         <span class="burger-line"></span>
+      </div>
+      <div class="menu" id="menu">
+         <ul class="menu-inner">
+            <li class="menu-item"><router-link to="/" class="menu-link">Home</router-link></li>
+            <li class="menu-item"><router-link to="#" class="menu-link">Feature</router-link></li>
+            <li class="menu-item"><router-link to="/products" class="menu-link">Products</router-link></li>
+            <li class="menu-item"><router-link to="/cart" class="menu-link">Cart</router-link></li>
+         </ul>
+      </div>
+      <router-link to="/login" class="menu-block">Login</router-link>
+   </nav>
+</header>
+</template>
+
+<style scoped>
+
+.header {
+	 position: sticky;
+	 top: 0;
+	 left: 0;
+	 width: 100%;
+	 height: auto;
+	 margin: 0 auto;
+	 transition: all 0.35s ease;
+	 background: #080a0c;
+	 color: #fff;
+}
+.on-scroll {
+	 background: #0c0f13;
+	 box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+ .navbar {
+	 display: flex;
+	 flex-direction: row;
+	 align-items: center;
+	 justify-content: flex-start;
+	 column-gap: 1.25rem;
+	 width: 100%;
+	 height: 4.25rem;
+	 margin: 0 auto;
+}
+ .brand {
+	 font-family: inherit;
+	 font-size: 1.6rem;
+	 font-weight: 600;
+	 line-height: 1.5;
+	 letter-spacing: -1px;
+	 color: #fff;
+	 text-transform: uppercase;
+}
+ .menu {
+	 position: fixed;
+	 top: -100%;
+	 left: 0;
+	 width: 100%;
+	 height: auto;
+	 padding: 4rem 0 3rem;
+	 overflow: hidden;
+	 background-color: #0c0f13;
+	 box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+	 transition: all 0.4s ease-in-out;
+}
+ .is-active {
+	 top: 0;
+	 width: 100%;
+	 height: auto;
+}
+ .menu-inner {
+	 display: flex;
+	 flex-direction: column;
+	 justify-content: center;
+	 align-items: center;
+	 row-gap: 1.25rem;
+}
+ .menu-link {
+	 font-family: inherit;
+	 font-size: 1rem;
+	 font-weight: 500;
+	 line-height: 1.5;
+	 color: #fff;
+	 text-transform: uppercase;
+	 transition: all 0.3s ease;
+}
+ .menu-block {
+	 display: inline-block;
+	 font-family: inherit;
+	 font-size: 0.875rem;
+	 font-weight: 500;
+	 line-height: 1.25;
+	 user-select: none;
+	 white-space: nowrap;
+	 text-align: center;
+	 margin-left: auto;
+	 padding: 0.65rem 1.5rem;
+	 border-radius: 3rem;
+	 text-transform: capitalize;
+	 color: #fff;
+	 background-color: #2463eb;
+	 box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+	 transition: all 0.3s ease-in-out;
+}
+ @media only screen and (min-width: 48rem) {
+	 .menu {
+		 position: relative;
+		 top: 0;
+		 width: auto;
+		 height: auto;
+		 padding: 0rem;
+		 margin-left: auto;
+		 background: none;
+		 box-shadow: none;
+	}
+	 .menu-inner {
+		 display: flex;
+		 flex-direction: row;
+		 column-gap: 2rem;
+		 margin: 0 auto;
+	}
+	 .menu-link {
+		 text-transform: capitalize;
+	}
+	 .menu-block {
+		 margin-left: 2rem;
+	}
+}
+ .burger {
+	 position: relative;
+	 display: block;
+	 cursor: pointer;
+	 user-select: none;
+	 order: -1;
+	 z-index: 10;
+	 width: 1.6rem;
+	 height: 1.15rem;
+	 border: none;
+	 outline: none;
+	 background: none;
+	 visibility: visible;
+	 transform: rotate(0deg);
+	 transition: 0.35s ease;
+}
+ @media only screen and (min-width: 48rem) {
+	 .burger {
+		 display: none;
+		 visibility: hidden;
+	}
+}
+ .burger-line {
+	 position: absolute;
+	 display: block;
+	 left: 0;
+	 width: 100%;
+	 height: 2px;
+	 border: none;
+	 outline: none;
+	 opacity: 1;
+	 border-radius: 1rem;
+	 transform: rotate(0deg);
+	 background-color: #fff;
+	 transition: 0.25s ease-in-out;
+}
+ .burger-line:nth-child(1) {
+	 top: 0px;
+}
+ .burger-line:nth-child(2) {
+	 top: 0.5rem;
+	 width: 70%;
+}
+ .burger-line:nth-child(3) {
+	 top: 1rem;
+}
+ .burger .is-active .burger-line:nth-child(1) {
+	 top: 0.5rem;
+	 transform: rotate(135deg);
+}
+ .burger .is-active .burger-line:nth-child(2) {
+	 opacity: 0;
+	 visibility: hidden;
+}
+ .burger .is-active .burger-line:nth-child(3) {
+	 top: 0.5rem;
+	 transform: rotate(-135deg);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,188 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<main class="homemain py-14">
+   <section class="section banner banner-section">
+      <div class="container banner-column">
+         <img class="banner-image" src="https://i.ibb.co/vB5LTFG/Headphone.png" alt="banner">
+         <div class="banner-inner">
+            <h1 class="heading-xl">Experience Media Like Never Before</h1>
+            <p class="paragraph">
+               Enjoy award-winning stereo beats with wireless listening freedom and sleek,
+               streamlined with premium padded and delivering first-rate playback.
+            </p>
+            <router-link to="/products" class="btn btn-darken btn-inline bg-[#101419]">
+               Our Products<i class="bx bx-right-arrow-alt"></i>
+            </router-link>
+         </div>
+         <div class="banner-links">
+            <router-link to="#" title=""><i class="bx bxl-facebook"></i></router-link>
+            <router-link to="#" title=""><i class="bx bxl-instagram"></i></router-link>
+            <router-link to="#" title=""><i class="bx bxl-twitter"></i></router-link>
+            <router-link to="#" title=""><i class="bx bxl-youtube"></i></router-link>
+         </div>
+      </div>
+   </section>
+</main>
+
 </template>
+<style scoped>
+.homemain{
+  background-color: #080a0c;
+  color: #fff;
+}
+.banner-section {
+	 margin: 0 auto;
+	 /* background-color: #080a0c; */
+
+}
+ .heading-lg {
+	 font-family: inherit;
+	 font-size: clamp(2.179rem, 5vw, 3.176rem);
+	 font-weight: 700;
+	 line-height: 1.15;
+	 letter-spacing: -1px;
+}
+.heading-xl {
+	 font-family: inherit;
+	 font-size: clamp(2.648rem, 6vw, 4.241rem);
+	 font-weight: 700;
+	 line-height: 1.15;
+	 letter-spacing: -1px;
+}
+.paragraph {
+	 font-family: inherit;
+	 font-size: clamp(1rem, 2vw, 1.125rem);
+	 text-wrap: balance;
+	 color: #e4e6e7;
+}
+.btn {
+	 display: inline-block;
+	 font-family: inherit;
+	 font-size: 1rem;
+	 font-weight: 500;
+	 line-height: 1.5;
+	 text-align: center;
+	 vertical-align: middle;
+	 white-space: nowrap;
+	 user-select: none;
+	 outline: none;
+   border: 1px solid transparent;
+	 border-radius: 0.25rem;
+	 text-transform: unset;
+	 transition: all 0.3s ease-in-out;
+}
+.btn:hover{
+   color: #fff;
+   border: 1px solid #2463eb;
+}
+ .btn-inline {
+	 display: inline-flex;
+	 align-items: center;
+	 justify-content: center;
+	 column-gap: 0.5rem;
+}
+ .btn-darken {
+	 padding: 0.75rem 2rem;
+	 color: #fff;
+	 /* background-color: #101419; */
+	 box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+ .banner-column {
+	 position: relative;
+	 display: grid;
+	 align-items: center;
+	 /* row-gap: 3rem; */
+}
+ @media only screen and (min-width: 48rem) {
+	 .banner-column {
+		 grid-template-columns: repeat(2, minmax(0, 1fr));
+		 justify-content: center;
+	}
+}
+ @media only screen and (min-width: 64rem) {
+	 .banner-column {
+		 grid-template-columns: 1fr max-content;
+		 column-gap: 4rem;
+		 /* margin-top: 3rem; */
+	}
+}
+ .banner-image {
+	 display: block;
+	 max-width: 18rem;
+	 height: auto;
+	 margin-top: 2rem;
+	 object-fit: cover;
+	 justify-self: center;
+}
+ @media only screen and (min-width: 48rem) {
+	 .banner-image {
+		 order: 1;
+		 max-width: 20rem;
+		 height: auto;
+	}
+}
+ @media only screen and (min-width: 64rem) {
+	 .banner-image {
+		 max-width: 25rem;
+		 height: auto;
+		 margin-right: 5rem;
+	}
+}
+ .banner-inner {
+	 display: flex;
+	 flex-direction: column;
+	 align-items: flex-start;
+	 row-gap: 1.75rem;
+}
+ .banner-links {
+	 position: absolute;
+	 top: 30%;
+	 right: 1.5rem;
+	 display: grid;
+	 justify-items: center;
+	 row-gap: 0.5rem;
+	 opacity: 0;
+	 visibility: hidden;
+}
+.banner-links:hover {
+  color: dodgerblue;
+}
+ @media only screen and (min-width: 64rem) {
+	 .banner-links {
+		 opacity: 1;
+		 visibility: visible;
+	}
+}
+ .banner-links > * {
+	 font-size: 1.25rem;
+	 line-height: 1.25;
+	 color: #fff;
+	 transition: all 0.3s ease-in-out;
+
+}
+.banner-links>*:hover {
+  color: #2463eb;
+}
+ .banner-links::before {
+	 position: absolute;
+	 content: "";
+	 top: -3rem;
+	 width: 4rem;
+	 height: 1.5px;
+	 transform: rotate(90deg);
+	 background: #fff;
+}
+ .banner-links::after {
+	 position: absolute;
+	 content: "";
+	 bottom: -3rem;
+	 width: 4rem;
+	 height: 2px;
+	 transform: rotate(90deg);
+	 background: #fff;
+}
+
+</style>
